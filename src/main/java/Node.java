55f709c94363c5174825fd739d8c0ff6d4c28993
Abdul Rizwan <abdulrizwan706@gmail.com>
2023-04-26 00:28:38 +0530

    this is it

--- src/main/java/Node.java
+++ src/main/java/Node.java
@@ -1,3 +1,18 @@
 public class Node {
     // write the code of node class here
+    // Two data members
+    // i. data:int
+    // ii. next:Node
+    // A constructor which takes an integer parameter. This constructor should set data property and leave the next set to null.
+    // A constructor which takes a Node parameter. This constructor should make a deep copy of the passed node.
+    int data;
+    Node next;
+    Node(int data){
+        this.data = data;
+    }
+
+    Node(Node clone){
+        this.data = clone.data;
+        this.next = clone.next;
+    }
 }
